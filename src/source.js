a513b9074d8e9400280ab3f9eecad6cb403e0e1a
William Gomez <89489831+WilliamG-1@users.noreply.github.com>
2023-03-04 00:13:59 -0800

    Added webpack html plugin, created a template in src as main.html

--- src/source.js
+++ src/source.js
@@ -1,9 +1,6 @@
-
+import './style.css';
 
 // Hello there
-
-console.log("Hello");
-alert("Hi ther~");
 
 const element = document.createElement('div');
 element.textContent = "Hi hahahhaha";
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 
 module.exports = {
+    mode: 'development',
     entry: './src/source.js',
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Restaurante',
+            template: 'src/main.html'
+        }),
+    ],
     module: {
         rules: [
             {
