fb2dd4b5dcf8079e537e9e27e5d9ccf13045b213
William Gomez <89489831+WilliamG-1@users.noreply.github.com>
2023-03-03 23:05:07 -0800

    moved index.html to dist

--- src/source.js
+++ src/source.js
@@ -1,5 +1,10 @@
-import './style.css'
+
+
 // Hello there
 
-
 console.log("Hello");
+alert("Hi ther~");
+
+const element = document.createElement('div');
+element.textContent = "Hi hahahhaha";
+document.body.appendChild(element);
